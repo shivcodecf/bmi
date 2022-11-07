d3c61972c002f376c2b07089c1bfcf5271bca0d9
shivcodecf <113226699+shivcodecf@users.noreply.github.com>
2022-11-07 23:19:52 +0530

    Update bmu.js

--- bmu.js
+++ bmu.js
@@ -24,7 +24,7 @@ button.addEventListener('click', () => {
         const bmi = (weight / ((height*height)/10000)).toFixed(2);
 
         if(bmi < 18.6){
-            result.innerHTML = 'Under weight : ' + bmi +   'weighing less than the normal amount for one's age, height, and build' ;
+            result.innerHTML = 'Under weight : ' + bmi + 'weighing less than the normal amount' ;
         }else if(bmi >= 18.6 && bmi < 24.9){
             result.innerHTML = 'Normal : ' + bmi;
         }else{
